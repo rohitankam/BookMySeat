3ecc2a4f8c449060da8205ec4f8186e250c8c037
rohitankam <rohitankam101@gmail.com>
2018-02-04 00:09:51 +0530

    asdasd

--- app/src/main/java/com/bookmyseat/rohit/bookmyseat/location.java
+++ app/src/main/java/com/bookmyseat/rohit/bookmyseat/location.java
@@ -6,6 +6,8 @@ import android.support.v7.widget.RecyclerView;
 
 public class location extends AppCompatActivity {
 
+
+    //asdfghjkl;
     RecyclerView r1;
     String s1[],s2[];
     int image[]={R.drawable.bahubali,R.drawable.tiger,R.drawable.conjuring};
@@ -15,12 +17,12 @@ public class location extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
 
-        r1=(RecyclerView)findViewById(R.id.recyclerView);
 
+
+        r1=(RecyclerView)findViewById(R.id.recyclerView);
         s1=getResources().getStringArray(R.array.movie_name);
         s2=getResources().getStringArray(R.array.movie_descp);
 
         la=new LocationAdapter(this,s1,s2,image);
     }
 }
-//hello
